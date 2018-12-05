3c7f91c24b2e588e2dcf4325926cdb1859d953be
Bastian Olavarria <btnolavarria@gmail.com>
2018-12-05 20:15:44 -0300

    final commit

---
+++ ExamenNET/Controllers/PedidoesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ExamenNET.Models;
+
+namespace ExamenNET.Controllers
+{
+    public class PedidoesController : Controller
+    {
+        private ExamenNetEntities db = new ExamenNetEntities();
+
+        // GET: Pedidoes
+        public ActionResult Index()
+        {
+            var pedido = db.Pedido.Include(p => p.Clientes).Include(p => p.Productos);
+            return View(pedido.ToList());
+        }
+
+        // GET: Pedidoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedido.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+
+        // GET: Pedidoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.Cliente = new SelectList(db.Clientes, "Rut", "Nombre");
+            ViewBag.Producto = new SelectList(db.Productos, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Pedidoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Cliente,Producto")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pedido.Add(pedido);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Cliente = new SelectList(db.Clientes, "Rut", "Nombre", pedido.Cliente);
+            ViewBag.Producto = new SelectList(db.Productos, "Id", "Nombre", pedido.Producto);
+            return View(pedido);
+        }
+
+        // GET: Pedidoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedido.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Cliente = new SelectList(db.Clientes, "Rut", "Nombre", pedido.Cliente);
+            ViewBag.Producto = new SelectList(db.Productos, "Id", "Nombre", pedido.Producto);
+            return View(pedido);
+        }
+
+        // POST: Pedidoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Cliente,Producto")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pedido).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Cliente = new SelectList(db.Clientes, "Rut", "Nombre", pedido.Cliente);
+            ViewBag.Producto = new SelectList(db.Productos, "Id", "Nombre", pedido.Producto);
+            return View(pedido);
+        }
+
+        // GET: Pedidoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedido.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+
+        // POST: Pedidoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pedido pedido = db.Pedido.Find(id);
+            db.Pedido.Remove(pedido);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
